c46b6bc8d107c0272368d225659f6e923b9821fc
jan-justin <u15073298@tuks.co.za>
2018-04-21 09:28:12 +0200

    Added scrollspy to navbar and smooth link scrolling

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   var SCROLL_PX_LIMIT = 85;
 
   /* Events */
+  // Un-blend the navbar from the content header when scrolling
   $(window).scroll(function () {
     /* 
      * If the user scrolls past a scroll limit,
@@ -16,4 +17,27 @@ $(document).ready(function () {
       $('.navbar-view-static').removeClass('navbar-view-static');
     }
   });
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      /*
+       * Using jQuery's animate() method to add smooth page scroll
+       * The optional number (800) specifies the number of milliseconds it 
+       * takes to scroll to the specified area
+       */
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
 });
