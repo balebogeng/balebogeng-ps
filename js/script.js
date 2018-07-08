75a840434924e69d14b067bf2e1885faa389fff1
jan-justin <u15073298@tuks.co.za>
2018-07-08 21:20:10 +0200

    Add gallery with images and library for gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
 $(document).ready(function () {
   /* Global vars */
   var SCROLL_PX_LIMIT = 65;
+  var GALLERY_CHANGE_INTERVAL = 5000;
+  var GALLERY_WINDOW_BREAKPOINT = 992;
+
+  var GALLERY_CONFIG = {
+    listPosition: 'left',
+    intervalDuration: GALLERY_CHANGE_INTERVAL
+  };
+  var GALLERY_MOBILE_CONFIG = {
+    displayList: false,
+    displayControls: true,
+    intervalDuration: GALLERY_CHANGE_INTERVAL
+  };
+
+  var galleryListHide = function(galleryClassSelector) {
+    var shouldHide = $(galleryClassSelector).css('visibility') === 'hidden';
+    shouldHide = shouldHide && pgwSlider.config.displayList;
+    return shouldHide;
+  }
+  var galleryListReShow = function(galleryClassSelector) {
+    var shouldReShow = $(galleryClassSelector).css('visibility') !== 'hidden';
+    shouldReShow = shouldReShow && !pgwSlider.config.displayList;
+    return shouldReShow;
+  }
+
+  var pgwSlider = $('.pgw-slider').pgwSlider(
+    $(window).width() > GALLERY_WINDOW_BREAKPOINT
+      ? GALLERY_CONFIG
+      : GALLERY_MOBILE_CONFIG
+  );
 
   /* Events */
   // Un-blend the navbar from the content header when scrolling
@@ -39,6 +68,16 @@ $(document).ready(function () {
       });
     } // End if
   });
+
+  // If a mobile breakpoint was triggered, reload the gallery for a more
+  // compact view.
+  $(window).resize(function() {
+    if (galleryListHide('.pgw-slider')) {
+      pgwSlider.reload(GALLERY_MOBILE_CONFIG);
+    } else if (galleryListReShow('.pgw-slider')) {
+      pgwSlider.reload(GALLERY_CONFIG)
+    }
+  }); 
 
   // Synchronize hover effect for footer sponsor logos
   $('.footer .sponsor a').on({
